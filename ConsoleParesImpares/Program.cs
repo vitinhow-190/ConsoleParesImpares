8d559b9be8f41115b63c59da79c5cea7ccbd06f9
Usuario <Usuario@CPD02-M07>
2024-03-26 11:06:14 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleParesImpares/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleParesImpares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+
+            Console.WriteLine("Digite 0 para selecionar a opção pares e 1 para impares: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int cont = num;
+            Console.Clear();         
+            
+                while (cont <=20)
+                {
+                   Console.WriteLine(cont);
+
+                    cont += 2;
+
+                }       
+
+            Console.ReadKey();
+        }
+    }
+}
